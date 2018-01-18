c55d7a7b7aabfdce37fbed4527e27a9d316bfc92
DESKTOP-BBE95OP\A <thevanica@gmail.com>
2018-01-17 20:42:47 -0500

    [Setting] Created a mastermanger to manage all of the other global mangers

---
+++ TheGladiator/Assets/Scripts/Manager/GlobalDataManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalDataManager : MonoBehaviour, IManager {
+
+	public void Initialize()
+    {
+        Debug.Log("boot Done " + typeof(GlobalDataManager));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TheGladiator/Assets/Scripts/Manager/MasterManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LoadSceneManager))]
+[RequireComponent(typeof(GlobalDataManager))]
+[RequireComponent(typeof(SoundManager))]
+
+public class MasterManager : MonoBehaviour {
+
+    private List<IManager> managerList = new List<IManager>();
+
+    public static LoadSceneManager  ManagerLoadScene    { get; private set; }
+    public static GlobalDataManager ManagerGlobalData   { get; private set; }
+    public static SoundManager      ManagerSound        { get; private set; }
+
+
+    private void Awake()
+    {
+        ManagerLoadScene = GetComponent<LoadSceneManager>();
+        ManagerGlobalData = GetComponent<GlobalDataManager>();
+        ManagerSound = GetComponent<SoundManager>();
+
+        //added all manager in the manager list
+        managerList.Add(ManagerLoadScene);
+        managerList.Add(ManagerGlobalData);
+        managerList.Add(ManagerSound);
+
+        StartCoroutine(IE_BootAllManager());
+
+        //keep this gameobject the entire project
+        DontDestroyOnLoad(gameObject);
+    }
+
+    IEnumerator IE_BootAllManager()
+    {
+        foreach(IManager manager in managerList)
+        {
+            manager.Initialize();
+        }
+
+        yield return null;
+    }
+}
