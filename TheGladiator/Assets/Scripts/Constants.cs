8953a60e3f6ef3f2e46b405c2ed905c9da245e57
DESKTOP-BBE95OP\A <thevanica@gmail.com>
2018-01-17 20:58:28 -0500

    [Setting] Created Constants and Utility scripts

---
+++ TheGladiator/Assets/Scripts/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this scripts is expected to be used for constant variables that we can have an access to this anywhere
+public static class Constants
+{
+    //example
+
+    public enum DayTimeType
+    {
+        MORNING = 0,
+        AFTERNOON,
+        NIGHT,
+    }
+
+    public static string playerName = "Hello Player";
+    public int PlayerIndex = 0;
+}
+
---
+++ TheGladiator/Assets/Scripts/Utility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this scripts is expected to be used for common function that we can have an access to this anywhere
+public static class Utility
+{
+    //example
+    public static string ConvertString(int number)
+    {
+        return number.ToString();
+    }
+}
