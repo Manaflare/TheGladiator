2ba3628724888eb3ae59fc5a66788365059c4821
SLCTECH\jlee <thevanica@gmail.com>
2018-01-26 09:54:50 -0500

    [Settings] Added Attribute Class

---
+++ TheGladiator/Assets/Scripts/Attribute.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct Status
+{
+    int Strength;
+    int Agility;
+    int Dexterity;
+}
+
+public class Attribute : MonoBehaviour
+{
+
+    public Status STATUS { get; private set; }
+    public float HP { get; private set; }
+    public float MAXHP { get; private set; }
+    public float MP { get; private set; }
+    public float MAXMP { get; private set; }
+    public bool IsAlive { get; private set; }
+    public bool IsDying { get; private set; }
+
+    // Use this for initialization
+    protected virtual void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    protected virtual void Update ()
+    {
+		
+	}
+}
---
+++ TheGladiator/Assets/Scripts/PlayerAttrbute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttrbute : Attribute
+{
+    public bool IsVulnerable { get; private set; }
+	// Use this for initialization
+	protected override void Start ()
+    {
+
+	}
+
+    // Update is called once per frame
+    protected override void Update ()
+    {
+		
+	}
+}
