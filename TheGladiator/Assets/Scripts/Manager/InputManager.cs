c6306e13859cf72c57fa5b0cd2862fd10a183023
SLCTECH\jlee <thevanica@gmail.com>
2018-01-26 11:16:24 -0500

    [Settings] Added Input and Town Manager

---
+++ TheGladiator/Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour, IManager {
+
+    // Use this for initialization
+    public void Initialize()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ TheGladiator/Assets/Scripts/Town/TownManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownManager : MonoBehaviour {
+
+    private static TownManager instance;
+    public static TownManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = GameObject.FindObjectOfType<TownManager>();
+                if(instance == null)
+                {
+                    GameObject container = new GameObject("TownManager");
+                    instance = container.AddComponent<TownManager>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
