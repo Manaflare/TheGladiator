0a8a2ef820d89f3442aeb38078d26e2afa561722
SLCTECH\jlee <thevanica@gmail.com>
2018-01-26 09:58:57 -0500

    [Settings] Added Character class structure

---
+++ TheGladiator/Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+	// Use this for initialization
+	protected virtual void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    protected virtual void Update ()
+    {
+		
+	}
+}
---
+++ TheGladiator/Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Character {
+
+    // Use this for initialization
+    protected override void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+
+    }
+}
