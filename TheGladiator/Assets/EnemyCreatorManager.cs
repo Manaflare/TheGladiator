2810396a5f80f7b5eec9ce756ae4784384ba71d6
fsimoes <fs.simoes@gmail.com>
2018-03-06 13:34:03 -0500

    Loading enemies

--- TheGladiator/Assets/EnemyCreatorManager.cs
+++ TheGladiator/Assets/EnemyCreatorManager.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyCreatorManager : CreateCharacterManager {
 
     List<ListDataInfo> enemyList;
-
+    public Dropdown enemyDropDown;
     // Use this for initialization
     void Start () {
+        enemyDropDown.ClearOptions();
         enemyList = MasterManager.ManagerGlobalData.GetEnemyDataInfo();
+
+        List<string> data = new List<string>();
+
+
+        for (int i = 0; i < enemyList.Count; i++)
+        {
+            data.Add(enemyList[i].statsList[0].Name + "_" + enemyList[i].playerTier);
+        }
+        enemyDropDown.AddOptions(data);
         Debug.Log(enemyList.ToString());
         Reset();
     }
