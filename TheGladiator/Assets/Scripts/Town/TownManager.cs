2badad31d72aa0886979712080277307b88f66ec
SLCTECH\jlee <thevanica@gmail.com>
2018-03-07 12:42:54 -0500

    [TC-54/Creaste Scene for town] Input process between buildings in the town scene

--- TheGladiator/Assets/Scripts/Town/TownManager.cs
+++ TheGladiator/Assets/Scripts/Town/TownManager.cs
@@ -37,22 +37,32 @@ public class TownManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		if(MasterManager.ManagerInput.GetKeyDown(KeyCode.UpArrow))
+        Objects[selectedIndex].GetComponent<GlowButton>().EndGlow();
+
+        if (MasterManager.ManagerInput.GetKeyDown(KeyCode.LeftArrow))
         {
-            Objects[selectedIndex].GetComponent<GlowButton>().EndGlow();
-            ++selectedIndex;
-            selectedIndex = Mathf.Min(selectedIndex, Objects.Length - 1);
-            Objects[selectedIndex].GetComponent<GlowButton>().StartGlow();
+            DecideSelectedObject(-1);
+        }
+        else if (MasterManager.ManagerInput.GetKeyDown(KeyCode.RightArrow))
+        {
+            DecideSelectedObject(1);
+
+        }
+        else if (MasterManager.ManagerInput.GetKeyDown(KeyCode.UpArrow))
+        {
+            DecideSelectedObject(-3);
+
+
         }
         else if (MasterManager.ManagerInput.GetKeyDown(KeyCode.DownArrow))
         {
-            Objects[selectedIndex].GetComponent<GlowButton>().EndGlow();
-            --selectedIndex;
-            selectedIndex = Mathf.Max(0, selectedIndex);
-            Objects[selectedIndex].GetComponent<GlowButton>().StartGlow();
+            DecideSelectedObject(3);
         }
 
-        if(MasterManager.ManagerInput.GetKeyDown(KeyCode.Return))
+
+        Objects[selectedIndex].GetComponent<GlowButton>().StartGlow();
+
+        if (MasterManager.ManagerInput.GetKeyDown(KeyCode.Return))
         {
             if (selectedIndex == Panels.Length)
             {
@@ -70,5 +80,37 @@ public class TownManager : MonoBehaviour {
             MasterManager.ManagerPopup.ShowMessageBox("TEST", "This is a test", Constants.PopupType.POPUP_NO);
             //for rest button
         }
+    }
+
+
+    void DecideSelectedObject(int increment)
+    {
+        /*
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0,   
+            
+            Array of buildings.
+            index of the array should be increased by 1 or 3 depends one what keyboard is pressed
+            i.e) uparrow or down arrow +- 3  left or right arrow +- 1
+         
+         */
+        int oldIndex = selectedIndex;
+
+        do
+        {
+            selectedIndex += increment;
+            //check if it is out of index
+            if (selectedIndex < 0 || selectedIndex > Objects.Length - 1)
+            {
+                selectedIndex = oldIndex;
+                break;
+            }
+        }
+        while (Objects[selectedIndex] == null);
+
+        //check if it's still null
+        if (Objects[selectedIndex] == null)
+            selectedIndex = oldIndex;
     }
 }
