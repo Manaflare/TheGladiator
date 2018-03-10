7b9e2c9ddd8c21bc571671653dff514fd2dea566
glasglow-PC\glasglow <jonah.virtanen@gmail.com>
2018-03-10 11:57:26 -0500

    [UI][Healer] Git issues fixed at home, Healer finally pushed properly

--- TheGladiator/Assets/Scripts/HealerScript.cs
+++ TheGladiator/Assets/Scripts/HealerScript.cs
@@ -3,24 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealerScript : MonoBehaviour {
+public class HealerScript : MonoBehaviour
+{
+
     public GameObject buttonLow;
     public GameObject buttonMid;
     public GameObject buttonHigh;
     public GameObject leaveButton;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void DestroyOnClick()
+	void Start ()
     {
+		ListDataInfo playerData = MasterManager.ManagerGlobalData.GetPlayerDataInfo();
+        //buttons are only usable if your tier is high enough; better healing options require you to be higher tier
+        if (playerData.playerTier == 1)
+        {
+            buttonLow.GetComponent<Button>().interactable = true;
+            buttonMid.GetComponent<Button>().interactable = false;
+            buttonHigh.GetComponent<Button>().interactable = false;
+        }
+        else if (playerData.playerTier == 2)
+        {
+            buttonLow.GetComponent<Button>().interactable = true;
+            buttonMid.GetComponent<Button>().interactable = true;
+            buttonHigh.GetComponent<Button>().interactable = false;
+        }
+        else if (playerData.playerTier == 3)
+        {
+            buttonLow.GetComponent<Button>().interactable = true;
+            buttonMid.GetComponent<Button>().interactable = true;
+            buttonHigh.GetComponent<Button>().interactable = true;
+        }
 
+        Button btn = leaveButton.GetComponent<Button>();
+        btn.onClick.AddListener(DestroyOnClick);
+    }
+
+    //closes the healer's page
+    public void DestroyOnClick()
+    {
+        Destroy(this.transform.root.gameObject);
     }
 }
