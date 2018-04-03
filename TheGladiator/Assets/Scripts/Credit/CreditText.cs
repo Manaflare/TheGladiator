b765c8f88a5f19a9b7cdc0747a3d6dfbeb5601fa
SLCTECH\jlee <thevanica@gmail.com>
2018-04-03 17:58:18 -0400

    [Credit] Working in progress

---
+++ TheGladiator/Assets/Scripts/Credit/CreditText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditText : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    private float speed = 1.5f;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RectTransform rt = GetComponent<RectTransform>();
+        if(rt.anchoredPosition.y <= -20)
+           GetComponent<RectTransform>().anchoredPosition = new Vector2(rt.anchoredPosition.x, rt.anchoredPosition.y + speed);
+        else
+        {
+
+        }
+	}
+
+}
