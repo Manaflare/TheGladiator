94b55bf234320ab903ccbbb84b1fe249946a94ee
SLCTECH\jlee <thevanica@gmail.com>
2018-04-13 11:22:17 -0400

    [Debug] put missing script

---
+++ TheGladiator/Assets/Scripts/Town/DebugConsole.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugConsole : MonoBehaviour {
+
+ 	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("DebugConsole");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+       
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "I am a button"))
+            print("You clicked the button!");
+
+    }
+
+    public void OnOpenDebugConsole()
+    {
+        Debug.Log("AasdSD");
+    }
+
+}
